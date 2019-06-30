6c6c847b41defb91e467ce975051892b0f6110ae
tinyhuang <tinyhuang@163.com>
2019-06-30 13:05:33 +0800

    start to use local pycharm to edit mycode

---
+++ python/python-practice.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@author: James Huang 
+@contact: huangjun_james@163.com
+@site: www.junworks.cn
+
+@version: 1.0
+@license: Apache Licence
+@file: python-practice.py
+@time: 2019/6/30 12:58
+
+这一行开始写关于本文件的说明与解释
+"""
+
+#test
+
+def run():
+    pass
+
+
+if __name__ == '__main__':
+    run()
